758879ec3582b25d35321d2c051ba5bbb6f0b3be
hojay <jeahonglee@gmail.com>
2024-04-13 17:51:14 +0900

    Task app complete

---
+++ app/prisma-example/page.js
@@ -0,0 +1,31 @@
+import prisma from '@/utils/db';
+
+const prismaHandlers = async () => {
+  // await prisma.task.create({
+  //   data: {
+  //     content: 'wake up',
+  //   },
+  // });
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+  return allTasks;
+};
+const PrismaPage = async () => {
+  const tasks = await prismaHandlers();
+  return (
+    <div>
+      <h1 className="text-7xl">Prisma</h1>
+      {tasks.map((task) => {
+        return (
+          <h2 key={task.id} className="text-xl py-2">
+            👩‍🦳{task.content}
+          </h2>
+        );
+      })}
+    </div>
+  );
+};
+export default PrismaPage;
